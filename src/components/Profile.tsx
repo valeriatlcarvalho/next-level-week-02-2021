22f56f6cee1aa66b2310eb2e6e9576eb3f300195
Valéria Carvalho <valeriatlcarvalhodev@outlook.com>
2021-02-25 22:27:22 -0300

    Primeiro commit

---
+++ src/components/Profile.tsx
@@ -0,0 +1,11 @@
+export function Profile() {
+  return (
+    <div>
+      <img src="https://github.com/valeriatlcarvalho" alt="Valéria Carvalho" />
+      <div>
+        <strong>Valéria Carvalho</strong>
+        <p>Level 1</p>
+      </div>
+    </div>
+  );
+}
